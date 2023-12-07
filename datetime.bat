a32986445fd3a78dbaf126ebebf3ad9009954c83
KhayalAsgerov <xeyalesgerov@gmail.com>
2023-12-07 10:46:36 +0400

    some changes

--- datetime.bat
+++ datetime.bat
@@ -1,4 +1,12 @@
-echo %DATE%
-echo %TIME%
-set datetimef=%date:~-4%_%date:~3,2%_%date:~0,2%__%time:~0,2%_%time:~3,2%_%time:~6,2%
-echo %datetimef%
+set hour=%time:~0,2%
+if "%hour:~0,1%" == " " set hour=0%hour:~1,1%
+echo hour=%hour%
+set min=%time:~3,2%
+if "%min:~0,1%" == " " set min=0%min:~1,1%
+echo min=%min%
+set secs=%time:~6,2%
+if "%secs:~0,1%" == " " set secs=0%secs:~1,1%
+echo secs=%secs%
+
+set year=%date:~-4%
+echo year=%year%
